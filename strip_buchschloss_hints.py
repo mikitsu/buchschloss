ebb15a30ac2181ab17892b6b00342527e1c66135
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2019-12-23 20:03:56 +0100

    only remove hints in Python files

--- strip_buchschloss_hints.py
+++ strip_buchschloss_hints.py
@@ -5,6 +5,8 @@ import strip_hints
 for dirpath, __, filenames in os.walk('buchschloss'):
     for filename in filenames:
         full_path = os.path.join(dirpath, filename)
+        if not full_path.endswith('.py'):
+            continue
         new_text = strip_hints.strip_file_to_string(full_path)
         with open(full_path, 'w', encoding='UTF-8') as f:
             f.write(new_text)
