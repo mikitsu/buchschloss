66e35dbf5ca9bf6a4e78bff7062bde8478890dde
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2020-04-30 16:31:37 +0200

    update config validation tests

--- test/config_test.py
+++ test/config_test.py
@@ -4,45 +4,45 @@ import datetime
 import base64
 from configobj import validate
 import pytest
-from buchschloss import config
+from buchschloss.config import validation as config_val
 
 
 def test_timedelta():
-    assert config.is_timedelta('1') == datetime.timedelta(days=1)
-    assert config.is_timedelta('1:1') == datetime.timedelta(days=1, hours=1)
-    assert config.is_timedelta('1:1:1') == datetime.timedelta(
+    assert config_val.is_timedelta('1') == datetime.timedelta(days=1)
+    assert config_val.is_timedelta('1:1') == datetime.timedelta(days=1, hours=1)
+    assert config_val.is_timedelta('1:1:1') == datetime.timedelta(
         days=1, hours=1, minutes=1)
-    assert config.is_timedelta('0:0:1.5') == datetime.timedelta(minutes=1, seconds=30)
+    assert config_val.is_timedelta('0:0:1.5') == datetime.timedelta(minutes=1, seconds=30)
     with pytest.raises(validate.ValidateError):
-        config.is_timedelta('1::1')
+        config_val.is_timedelta('1::1')
     with pytest.raises(validate.ValidateError):
-        config.is_timedelta('1:1:1:1')
+        config_val.is_timedelta('1:1:1:1')
     with pytest.raises(validate.ValidateError):
-        config.is_timedelta(':1:1')
+        config_val.is_timedelta(':1:1')
     with pytest.raises(validate.ValidateError):
-        config.is_timedelta(':')
+        config_val.is_timedelta(':')
 
 
 def test_optionlist():
-    assert config.is_optionlist(list('123'), '1', '2', '3') == list('123')
-    assert config.is_optionlist(list('aea'), 'a', 'e', 'i', 'o', 'u') == list('aea')
+    assert config_val.is_optionlist(list('123'), '1', '2', '3') == list('123')
+    assert config_val.is_optionlist(list('aea'), 'a', 'e', 'i', 'o', 'u') == list('aea')
     with pytest.raises(validate.ValidateError):
-        config.is_optionlist(list('123'), '1', '2', 'not 3')
+        config_val.is_optionlist(list('123'), '1', '2', 'not 3')
     with pytest.raises(validate.ValidateError):
-        config.is_optionlist(['asdf', 'ghjk'], *list('asdfghjk'))
+        config_val.is_optionlist(['asdf', 'ghjk'], *list('asdfghjk'))
 
 
 def test_base64bytes():
-    assert config.is_base64bytes(base64.b64encode(b'\x00\t').decode()) == b'\x00\t'
-    assert config.is_base64bytes(base64.b64encode(b'\x00\a').decode(), length=2) == b'\x00\a'
+    assert config_val.is_base64bytes(base64.b64encode(b'\x00\t').decode()) == b'\x00\t'
+    assert config_val.is_base64bytes(base64.b64encode(b'\x00\a').decode(), length=2) == b'\x00\a'
     with pytest.raises(validate.ValidateError):
-        config.is_base64bytes(base64.b64encode(b'\x00\a\t').decode(), length=2)
+        config_val.is_base64bytes(base64.b64encode(b'\x00\a\t').decode(), length=2)
     with pytest.raises(validate.ValidateError):
-        config.is_base64bytes(base64.b64encode(b'\x00\t\a').decode()+'a')
+        config_val.is_base64bytes(base64.b64encode(b'\x00\t\a').decode()+'a')
     with pytest.raises(validate.ValidateError):
-        config.is_base64bytes(['a', 'b'])
+        config_val.is_base64bytes(['a', 'b'])
     with pytest.raises(validate.ValidateError):
-        config.is_base64bytes('\tA\a')
+        config_val.is_base64bytes('\tA\a')
 
 
 # test tasklist later when it's more than just an optionlist wrapper
