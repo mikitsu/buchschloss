28fa5b2ab1cc7a1f8b406f330d190045c7343d25
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2020-05-01 10:18:35 +0200

    change models.Person and add migration

---
+++ migrations/person_pay_date_to_borrow_permission.py
@@ -0,0 +1,21 @@
+"""Rename Person.pay_date to Person.borrow_permission and set forward 52 weeks"""
+
+from playhouse import migrate
+import sys
+
+try:
+    db_name = sys.argv[1]
+except IndexError:
+    db_name = input('database path -> ')
+db = migrate.SqliteDatabase(db_name)
+migrator = migrate.SqliteMigrator(db)
+
+migrate.migrate(
+    migrator.rename_column('person', 'pay_date', 'borrow_permission')
+)
+
+db.execute_sql("""
+UPDATE person 
+    SET borrow_permission = date(borrow_permission, "364 days")
+    WHERE borrow_permission IS NOT NULL
+""")
