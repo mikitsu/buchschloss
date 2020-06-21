cdb79ea72b4be30d15947957edee8e1544acfd0b
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2020-06-21 18:43:10 +0200

    update leseclub.lua to fit new script API

--- scripts/leseclub.lua
+++ scripts/leseclub.lua
@@ -3,25 +3,67 @@ Leseclub management
 ]]--
 -- TODO: add some kind of error handling
 
-local borrow_weeks = config['borrow weeks']
+local borrow_weeks = tonumber(config['borrow weeks'])
+if borrow_weeks == nil then
+    error('missing configuration value "borrow weeks"')
+end
+local lc_library_name = config['library name'] or 'leseclub'
 
 local storage = buchschloss.get_storage()
 
-function borrow()
+
+local function check_leseclub_active(wanted_active)
+    local is_active
+    if storage.pending_borrows and storage.read_books then
+        is_active = true
+    else if not (storage.pending_borrows or storage.read_books) then
+        is_active = false
+    else
+        error('storage corrupt')
+    end end
+    if wanted_active == is_active then
+        return false
+    else
+        if is_active then
+            ui.alert('leseclub_active')
+        else
+            ui.alert('leseclub_not_active')
+        end
+        return true
+    end
+end
+
+local function check_book_in_lc_library(book)
+    if Book[book].library.name == lc_library_name then
+        return false
+    else
+        ui.alert('book_not_in_leseclub_library')
+        return true
+    end
+end
+
+local function borrow()
+    if check_leseclub_active(true) then return end
     local data = ui.get_data{book='int', person='int'}
+    if check_book_in_lc_library(data.book) then return end
     data.weeks = borrow_weeks
     Borrow:new(data)
 end
 
-function restitute()
+local function restitute()
+    if check_leseclub_active(true) then return end
     local data = ui.get_data{book='int', person='int', points='int'}
-    local ret = Borrow:restitute{book=data.book, person=data.person}
-    storage.read_books[person] = (storage.read_books[person] or 0) + data.points
+    if check_book_in_lc_library(data.book) then return end
+    local person = data.person
+    local ret = Borrow:restitute{book=data.book, person=person}
+    local new_points = (storage.read_books[tostring(person)] or 0) + data.points
+    storage.read_books[tostring(person)] = new_points
     buchschloss.set_storage(storage)
-    ui.alert(ret)
+    ui.alert('restitute_success_{}', ret)
 end
 
-function get_results()
+local function get_results()
+    if check_leseclub_active(true) then return end
     local r = {}
     for k, v in pairs(storage.read_books) do
         r[Person[k].__str__] = v
@@ -29,14 +71,16 @@ function get_results()
     ui.display(r)
 end
 
-function start_leseclub()
+local function start_leseclub()
+    if check_leseclub_active(false) then return end
     storage.pending_borrows = {}
     storage.read_books = {}
     buchschloss.set_storage(storage)
     ui.alert('leseclub_started')
 end
 
-function end_leseclub()
+local function end_leseclub()
+    if check_leseclub_active(true) then return end
     if not ui.ask('really_end_leseclub') then
         return
     end
@@ -46,13 +90,11 @@ function end_leseclub()
     ui.alert('leseclub_ended')
 end
 
-ui.register_action('borrow', borrow)
-ui.register_action('restitute', restitute)
-if storage.pending_borrows and storage.read_books then
-    ui.register_action('end_leseclub', end_leseclub)
-    ui.register_action('get_results', get_results)
-else if not (storage.pending_borrows or storage.read_books) then
-    ui.register_action('start_leseclub', start_leseclub)
-else
-    error('storage corrupt')
-end end
+
+return {
+    start = start_leseclub,
+    ['end'] = end_leseclub,
+    get_results = get_results,
+    borrow = borrow,
+    restitute = restitute,
+}
