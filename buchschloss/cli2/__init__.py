17e067470474f4a45dca1ad5d8c0615215b482ca
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2020-06-21 18:11:30 +0200

    avoid sorting table keys; they may be unsortable

--- buchschloss/cli2/__init__.py
+++ buchschloss/cli2/__init__.py
@@ -35,8 +35,8 @@ def data_to_table(runtime, data):
 def table_to_data(table):
     """convert a Lua table to a dict or a list"""
     if lupa.lua_type(table) == 'table':
-        keys = sorted(table.keys())
-        if keys == list(range(1, len(keys)+1)):
+        keys = set(table.keys())
+        if keys == set(range(1, len(keys)+1)):
             return [table_to_data(t) for t in table.values()]
         else:
             return {k: table_to_data(v) for k, v in table.items()}
