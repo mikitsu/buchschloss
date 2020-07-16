2191184de6e3e3f725095e75f4d61debc62c1702
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2020-07-16 17:58:55 +0200

    allow keys to be used for OptionsFromSearch setting so that it works with default entries and when setting values from the database

--- buchschloss/gui2/widgets.py
+++ buchschloss/gui2/widgets.py
@@ -101,7 +101,7 @@ class OptionsFromSearch(mtk.OptionChoiceWidget):
         super().__init__(master, values=values, **kwargs)
 
     def set(self, value):
-        return super().set(getattr(value, self.__attribute))
+        return super().set(getattr(value, self.__attribute, value))
 
 
 @mtk.ScrollableWidget(height=config.gui2.widget_size.main.height,
